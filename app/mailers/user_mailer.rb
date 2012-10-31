3b103918bcc6dbb79ca5a00538ed8f0f27a2b093
Sean Eshbaugh <seaneshbaugh@gmail.com>
2012-10-31 09:08:11 -0500

    Cleaned everything out.

--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default :from => "sean@seaneshbaugh.com"
-
-  def password_reset(user)
-    @user = user
-
-    mail :to => user.email_address, :subject => "seaneshbaugh.com - Password Reset"
-  end
-end
