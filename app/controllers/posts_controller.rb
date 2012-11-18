84b0a7d34713270d08165f9d255dd1371d11cda1
Sean Eshbaugh <seaneshbaugh@gmail.com>
2012-11-18 06:17:29 -0600

    Changed sort order for posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.where(:visible => true).order('created_at DESC')
   end
 
   def show
-    @post = Post.where(:slug => params[:id]).first
+    @post = Post.where(:slug => params[:id], :visible => true).first
 
     if @post.nil?
       flash[:error] = t('messages.posts.could_not_find')
