f2f8af692377533bc77e83e9dc21dc8766d07f3d
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-09-18 13:46:37 -0500

    Fixed Kaminari pagination.

---
+++ config/initializers/kaminari_custom_routes.rb
@@ -0,0 +1,36 @@
+require 'active_support/configurable'
+
+module Kaminari
+  class Configuration #:nodoc:
+    config_accessor :route
+  end
+
+  configure do |config|
+    config.route = :url_for
+  end
+
+  module ActionViewExtension
+    module InstanceMethods
+      def paginate(scope, options = {}, &block)
+        paginator = Kaminari::Helpers::Paginator.new self, options.reverse_merge(:current_page => scope.current_page, :num_pages => scope.num_pages, :per_page => scope.limit_value, :param_name => Kaminari.config.param_name, :remote => false, :route => :url_for)
+        paginator.to_s
+      end
+    end
+  end
+
+  module Helpers
+    class Tag
+      def initialize(template, options = {}) #:nodoc:
+        @template, @options = template, options.dup
+        @param_name = @options.delete(:param_name)
+        @route = @options.delete(:route)
+        @theme = @options[:theme] ? "#{@options.delete(:theme)}/" : ''
+        @params = @options[:params] ? template.params.merge(@options.delete :params) : template.params
+      end
+
+      def page_url_for(page)
+        @template.send(@route, @params.merge(@param_name => (page <= 1 ? nil : page)))
+      end
+    end
+  end
+end
