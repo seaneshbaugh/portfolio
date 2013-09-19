f866fff5e1b6406312d48a9bc06b9c2bced9f94d
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-09-19 16:49:28 -0500

    Started fixing admin panel.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,7 @@ module ApplicationHelper
 
   def build_url_or_path_for url_or_path = ''
     url_or_path = eval(url_or_path) if url_or_path =~ /_path|_url|@/
+
     url_or_path
   end
 
@@ -17,11 +18,11 @@ module ApplicationHelper
   end
 
   def is_active_controller? controller_name
-    'active' if params[:controller] == controller_name
+    params[:controller] == controller_name
   end
 
   def is_active_action? action_name
-    'active' if params[:action] == action_name
+    params[:action] == action_name
   end
 
   def date_range form
