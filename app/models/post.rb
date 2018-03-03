c746f26fb0da087956bbed56453afb57763316f5
Sean Eshbaugh <seaneshbaugh@gmail.com>
2018-03-03 04:22:14 -0600

    Updated Post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,57 +1,27 @@
-class Post < ActiveRecord::Base
+# frozen_string_literal: true
+
+class Post < ApplicationRecord
   include OptionsForSelect
   include Slugable
 
-  # Scopes
   scope :alphabetical, -> { order(:title) }
-
   scope :chronological, -> { order(:created_at) }
-
   scope :published, -> { where(visible: true) }
+  scope :reverse_alphabetical, -> { order(title: :desc) }
+  scope :reverse_chronological, -> { order(created_at: :desc) }
+  scope :unpublished, -> { where(visible: false) }
 
-  scope :reverse_alphabetical, -> { order('posts.title DESC') }
-
-  scope :reverse_chronological, -> { order('posts.created_at DESC') }
-
-  # Associations
   belongs_to :user
 
-  # Validations
-  validates_presence_of :user_id
-
-  validates_length_of :title, maximum: 255
-  validates_presence_of :title
-  validates_uniqueness_of :title
-
-  validates_length_of :body, maximum: 16_777_215
-
-  validates_length_of :style, maximum: 4_194_303
-
-  validates_length_of :script, maximum: 4_194_303
-
-  validates_length_of :meta_description, maximum: 65535
-
-  validates_length_of :meta_keywords, maximum: 65535
-
-  validates_inclusion_of :visible, in: [true, false], message: 'must be true or false'
-
+  validates :user_id, presence: true
   validates_associated :user
-
-  # Default Values
-  default_value_for :title, ''
-
-  default_value_for :slug, ''
-
-  default_value_for :body, ''
-
-  default_value_for :style, ''
-
-  default_value_for :script, ''
-
-  default_value_for :meta_description, ''
-
-  default_value_for :meta_keywords, ''
-
+  validates :title, presence: true, length: { maximum: 255 }, uniqueness: true
+  validates :body, presence: true, length: { maximum: 16_777_215 }
+  validates :style, presence: true, length: { maximum: 4_194_303 }
+  validates :script, presence: true, length: { maximum: 4_194_303 }
+  validates :meta_description, presence: true, length: { maximum: 65535 }
+  validates :meta_keywords, presence: true, length: { maximum: 65535 }
+  validates :visible, inclusion: { in: [true, false] }
   default_value_for :visible, true
 
   acts_as_taggable
