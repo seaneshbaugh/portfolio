d75cd51abe143bd4268f6aceec31e88377a5f7ee
Sean Eshbaugh <seaneshbaugh@gmail.com>
2018-03-03 04:29:10 -0600

    Updated User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,55 +1,31 @@
-class User < ActiveRecord::Base
+# frozen_string_literal: true
+
+class User < ApplicationRecord
   include OptionsForSelect
 
-  # Scopes
-  Ability::ROLES.each do |role_name, _|
-    scope role_name.to_s.pluralize.to_sym, -> { where(role: Ability::ROLES[role_name].downcase) }
+  scope :alphabetical, -> { order(:last_name, :first_name) }
+  scope :reverse_alphabetical, -> { order(last_name: :desc, first_name: :desc) }
+
+  %w[read_only user admin sysadmin].each do |role_name|
+    scope role_name.to_s.pluralize.to_sym, -> { where(role: role_name) }
   end
 
-  scope :alphabetical, -> { order(:last_name, :first_name) }
-
-  scope :reverse_alphabetical, -> { order('users.last_name DESC, users.first_name DESC') }
-
-  # Associations
   has_many :posts, dependent: :destroy
 
-  # Validations
-  validates :email, email: { strict_mode: true }, allow_blank: true
-  validates_presence_of :email
-  validates_uniqueness_of :email, case_sensitive: false
+  validates :email, presence: true, email: { allow_blank: true }, uniqueness: { case_sensitive: false }
+  validates :role, presence: true, inclusion: { in: %w[read_only user admin sysadmin] }
+  validates :first_name, presence: true, length: { maximum: 255 }
+  validates :last_name, presence: true, length: { maximum: 255 }
 
-  validates_confirmation_of :password
-  validates_length_of :password, within: 6..255, if: :password_required?
-  validates_presence_of :password, if: :password_required?
-
-  validates_inclusion_of :role, in: Ability::ROLES.map { |_, role_value| role_value }
-  validates_presence_of :role
-
-  validates_length_of :first_name, maximum: 255
-  validates_presence_of :first_name
-
-  validates_length_of :last_name, maximum: 255
-  validates_presence_of :last_name
-
-  # Callbacks
   before_save :define_role
 
-  # Default Values
-  default_value_for :email, ''
-
-  default_value_for :encrypted_password, ''
-
-  default_value_for :role, Ability::ROLES[:read_only]
-
-  default_value_for :first_name, ''
-
-  default_value_for :last_name, ''
+  default_value_for :role, 'read_only'
 
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
-  has_paper_trail only: [:email, :role, :first_name, :last_name]
+  has_paper_trail only: %i[email role first_name last_name]
 
-  Ability::ROLES.each do |role_name, _|
+  %w[read_only user admin sysadmin].each do |role_name|
     define_method("#{role_name}?") do
       role == role_name.to_s
     end
@@ -57,10 +33,6 @@ class User < ActiveRecord::Base
     define_method("#{role_name}!") do
       self.role = role_name.to_s
     end
-  end
-
-  def ability
-    @ability ||= Ability.new(self)
   end
 
   def full_name
@@ -73,11 +45,7 @@ class User < ActiveRecord::Base
 
   protected
 
-  def password_required?
-    !persisted? || !password.blank? || !password_confirmation.blank?
-  end
-
   def define_role
-    self.role = Ability::ROLES.include?(role.downcase.to_sym) ? role.downcase : Ability::ROLES[:read_only]
+    self.role = %w[read_only user admin sysadmin].include?(role.to_s.downcase) ? role.to_s.downcase : 'read_only'
   end
 end
