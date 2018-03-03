2ff03f14077c493b021f1aa8fdad0427e66ef9d1
Sean Eshbaugh <seaneshbaugh@gmail.com>
2018-03-03 04:04:51 -0600

    Fixed typo. Not sure if i18n.load_path stuff is supposed to go here or in an initializer. I need to test that.

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,6 @@ module Portfolio
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}']
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
