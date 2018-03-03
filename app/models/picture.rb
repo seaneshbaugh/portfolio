e12f31b4ce62590b8b1f4afe896056381ff976e2
Sean Eshbaugh <seaneshbaugh@gmail.com>
2018-03-03 13:41:20 -0600

    Updated Picture.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,53 +1,22 @@
-class Picture < ActiveRecord::Base
-  has_attached_file :image,
-                    path: :attachment_path,
-                    styles: -> (_) { attachment_styles },
-                    url: :attachment_url
+# frozen_string_literal: true
 
-  # Scopes
+class Picture < ApplicationRecord
+  has_attached_file :image, path: :attachment_path, styles: ->(_) { attachment_styles }, url: :attachment_url
+
   scope :chronological, -> { order(:created_at) }
+  scope :reverse_chronological, -> { order(created_at: :desc) }
 
-  scope :reverse_chronological, -> { order('pictures.created_at DESC') }
-
-  # Validations
-  validates_length_of :title, maximum: 65535
-  validates_presence_of :title
-
-  validates_length_of :alt_text, maximum: 65535
-
-  validates_length_of :caption, maximum: 65535
-
-  validates_uniqueness_of :image_fingerprint, if: -> { !Rails.env.test? }
-
+  validates :title, presence: true, length: { maximum: 65535 }
+  validates :alt_text, length: { maximum: 65535 }
+  validates :caption, length: { maximum: 65535 }
+  validates :image_fingerprint, uniqueness: { if: -> { !Rails.env.test? } }
   validates_attachment_presence :image
   validates_attachment_size :image, less_than: 1024.megabytes
-  validates_attachment_content_type :image, content_type: %w(image/gif image/jpeg image/jpg image/pjpeg image/png image/svg+xml image/tiff image/x-png)
+  validates_attachment_content_type :image, content_type: %w[image/gif image/jpeg image/jpg image/pjpeg image/png image/svg+xml image/tiff image/x-png]
 
-  # Callbacks
-  before_validation :modify_image_file_name
-
-  before_validation :set_default_title
-
+  before_validation :ensure_title
+  before_validation :normalize_image_file_name
   after_post_process :save_image_dimensions
-
-  # Default Values
-  default_value_for :title, ''
-
-  default_value_for :alt_text, ''
-
-  default_value_for :caption, ''
-
-  default_value_for :image_original_width, 1
-
-  default_value_for :image_original_height, 1
-
-  default_value_for :image_medium_width, 1
-
-  default_value_for :image_medium_height, 1
-
-  default_value_for :image_thumb_width, 1
-
-  default_value_for :image_thumb_height, 1
 
   def self.attachment_styles
     {
@@ -72,9 +41,17 @@ class Picture < ActiveRecord::Base
     "/uploads/:class_singular/:attachment/#{Rails.env.test? ? 'test/' : ''}:style_prefix:basename.:extension"
   end
 
-  protected
+  private
 
-  def modify_image_file_name
+  def default_title
+    File.basename(image_file_name, '.*').to_s
+  end
+
+  def ensure_title
+    self.title = default_title if title.blank? && image_file_name.present?
+  end
+
+  def normalize_image_file_name
     return unless image.file? && image.dirty?
 
     current_time = Time.now
@@ -87,24 +64,23 @@ class Picture < ActiveRecord::Base
 
     extension = '.tiff' if extension == '.tif'
 
-    image.instance_write :file_name, "#{basename}#{extension}"
+    image.instance_write(:file_name, "#{basename}#{extension}")
   end
 
   def save_image_dimensions
     original_geometry = Paperclip::Geometry.from_file(image.queued_for_write[:original])
+
     self.image_original_width = original_geometry.width
     self.image_original_height = original_geometry.height
 
     medium_geometry = Paperclip::Geometry.from_file(image.queued_for_write[:medium])
+
     self.image_medium_width = medium_geometry.width
     self.image_medium_height = medium_geometry.height
 
     thumb_geometry = Paperclip::Geometry.from_file(image.queued_for_write[:thumb])
+
     self.image_thumb_width = thumb_geometry.width
     self.image_thumb_height = thumb_geometry.height
-  end
-
-  def set_default_title
-    self.title = File.basename(image_file_name, '.*').to_s if title.blank? && !image_file_name.blank?
   end
 end
