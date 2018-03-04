9c68bf031dad47d648eb7aef5950f57461ca3ceb
Sean Eshbaugh <seaneshbaugh@gmail.com>
2018-03-04 04:32:53 -0600

    Removed role from User.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304072015) do
+ActiveRecord::Schema.define(version: 20180304102914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,7 +112,6 @@ ActiveRecord::Schema.define(version: 20180304072015) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "role", default: "", null: false
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
     t.string "reset_password_token"
