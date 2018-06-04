e648d5d5045c76853f94e098640232048cd82770
Sean Eshbaugh <seaneshbaugh@gmail.com>
2018-06-03 20:11:31 -0500

    Fixed argument bug.

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
+# TODO: Consider moving this to a proper API with some sort of token based authentication.
 class Admin::TagsController < Admin::AdminController
   def index
     @search = ActsAsTaggableOn::Tag.ransack(params[:q])
 
     @tags = @search.result
 
-    render json: @tags.map(:name).to_json
+    render json: @tags.map(&:name).to_json
   end
 end
