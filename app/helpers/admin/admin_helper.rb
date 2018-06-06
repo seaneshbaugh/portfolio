135b338f80d6df1aa348bc3b6018ce5419ebaf79
Sean Eshbaugh <seaneshbaugh@gmail.com>
2018-06-05 20:50:55 -0500

    Now with more newlines.

--- app/helpers/admin/admin_helper.rb
+++ app/helpers/admin/admin_helper.rb
@@ -5,7 +5,13 @@ module Admin
     def gem_dependencies
       lockfile = Bundler::LockfileParser.new(Bundler.read_file(Rails.root.join('Gemfile.lock')))
 
-      lockfile.specs.map { |spec| { name: spec.name, version: spec.version.version, dependencies: spec.dependencies.map { |dependency| { name: dependency.name, requirement: dependency.requirement } } } }
+      lockfile.specs.map do |spec|
+        {
+          name: spec.name,
+          version: spec.version.version,
+          dependencies: spec.dependencies.map { |dependency| { name: dependency.name, requirement: dependency.requirement } }
+        }
+      end
     end
   end
 end
