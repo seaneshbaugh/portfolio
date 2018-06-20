2a55a010f1f859fae988cc985176db9400873010
Sean Eshbaugh <seaneshbaugh@gmail.com>
2018-06-19 21:30:16 -0500

    Material Icons.

--- app/javascript/shared/index.js
+++ app/javascript/shared/index.js
@@ -1,6 +1,7 @@
 import Rails from "rails-ujs";
 
 import "./images";
-import "./javascripts/form";
+import "./javascripts";
+import "./stylesheets/material_icons";
 
 Rails.start();
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader',
+  options: {
+    attempts: 1
+  }
+});
+
+module.exports = environment;
