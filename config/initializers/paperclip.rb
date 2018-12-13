f7eaee6744e5cce0c6375b0dcf8335d5ddd98902
Sean Eshbaugh <seaneshbaugh@gmail.com>
2018-12-12 22:20:41 -0600

    Removed paperclip config.

--- config/initializers/paperclip.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Paperclip.interpolates :style_prefix do |attachment, style|
-#   style == :original ? '' : "#{style.to_s}_"
-# end
-
-# Paperclip.interpolates :class_singular do |attachment, style|
-#   attachment.instance.class.to_s.underscore
-# end
