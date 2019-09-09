ac79fc37812f0f0ef3254eceed8bfa141a3e8aeb
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-09-08 19:42:41 -0500

    Updated config files.

--- config/initializers/postgresql_database_tasks.rb
+++ config/initializers/postgresql_database_tasks.rb
@@ -6,7 +6,7 @@ module ActiveRecord
       def drop
         establish_master_connection
 
-        connection.select_all "select pg_terminate_backend(pg_stat_activity.pid) from pg_stat_activity where datname='#{configuration['database']}' AND state='idle';"
+        connection.select_all "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE datname='#{configuration['database']}' AND state='idle';"
 
         connection.drop_database configuration['database']
       end
