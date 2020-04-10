ce301a58520f95e1df612945c9b989e6bc94ce6f
Sean Eshbaugh <seaneshbaugh@gmail.com>
2020-04-10 18:16:15 -0500

    Disable contact form.

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -6,19 +6,19 @@ class ContactController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(contact_params).sanitize!
+    # @contact = Contact.new(contact_params).sanitize!
 
-    if @contact.valid?
-      ContactJob.perform_later(@contact.to_json)
+    # if @contact.valid?
+    #   ContactJob.perform_later(@contact.to_json)
 
-      render 'thanks'
-    elsif @contact.errors[:body].include?('is spam')
-      render 'thanks'
-    else
-      flash.now[:error] = helpers.error_messages_for(@contact)
+    #   render 'thanks'
+    # elsif @contact.errors[:body].include?('is spam')
+    render 'thanks'
+    # else
+    #   flash.now[:error] = helpers.error_messages_for(@contact)
 
-      render 'new', status: :unprocessable_entity
-    end
+    #   render 'new', status: :unprocessable_entity
+    # end
   end
 
   private
