440ef373bbfa4f6c1ab147f7e3b18372c20a2d1e
Sean Eshbaugh <seaneshbaugh@gmail.com>
2025-04-28 09:54:46 -0500

    Added health check to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
   devise_for :users, skip: %i[sessions passwords registrations confirmations unlocks]
 
   devise_scope :user do
@@ -64,7 +74,8 @@ Rails.application.routes.draw do
     post "versions/:id/revert" => "versions#revert", as: :revert_version
   end
 
-  root to: "posts#index"
+  # Defines the root path route ("/")
+  root "posts#index"
 
   get ":id" => "pages#show", as: :page
 end
