a52d342b3c4a6b969b6ad8c4a1fc36622b4fb88d
Sean Eshbaugh <seaneshbaugh@gmail.com>
2025-05-11 16:52:02 -0500

    Use actual babel transform packages instead of proposal versions.

--- babel.config.js
+++ babel.config.js
@@ -49,13 +49,13 @@ module.exports = function(api) {
       isTestEnv && require('babel-plugin-dynamic-import-node'),
       require('@babel/plugin-transform-destructuring').default,
       [
-        require('@babel/plugin-proposal-class-properties').default,
+        require('@babel/plugin-transform-class-properties').default,
         {
           loose: true
         }
       ],
       [
-        require('@babel/plugin-proposal-object-rest-spread').default,
+        require('@babel/plugin-transform-object-rest-spread').default,
         {
           useBuiltIns: true
         }
