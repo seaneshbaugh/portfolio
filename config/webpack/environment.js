5c0f4ba2c4cfadcbab5367b0839cc88a66039b44
Sean Eshbaugh <seaneshbaugh@gmail.com>
2025-05-11 20:40:03 -0500

    Remove unneeded webpacker configs. config/webpack/webpack.config.js is the only config file needed now.

--- config/webpack/environment.js
+++
@@ -1,8 +0,0 @@
-const { environment } = require('@rails/webpacker');
-
-environment.loaders.get('sass').use.splice(-1, 0, {
-  loader: 'resolve-url-loader',
-  options: {}
-});
-
-module.exports = environment;
