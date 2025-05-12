71f00082c0ca195fd11137925de02285ed3127e6
Sean Eshbaugh <seaneshbaugh@gmail.com>
2025-05-11 20:43:51 -0500

    Add custom sass loader config to make dependencies not noisy and silence global-builtin deprecation warnings from materializecss. Apparently this is how you're supposed to override the configs for the default loaders that shakapacker supports. It's kinda weird but it makes sense. See the github issue in the comment for confirmation. Not sure why the resolve extensions is needed but nothing will load without it. You'd think that'd be covered by the defaults. I should check the documentation.

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,6 +1,25 @@
 // See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
-const { generateWebpackConfig } = require('shakapacker')
+const { generateWebpackConfig, merge } = require("shakapacker");
 
-const webpackConfig = generateWebpackConfig()
+const options = {
+  resolve: {
+    // Not gonna lie, I'm still not 100% sure why this is needed if shakapacker.yml defines extensions.
+    extensions: [".jpg", ".jpeg", ".gif", ".js", ".jsx", ".png", ".scss"]
+  }
+};
 
-module.exports = webpackConfig
+const webpackConfig = generateWebpackConfig(options);
+
+// Override default sass-loader config. This is apparently how it's supposed to be done.
+// See https://github.com/shakacode/shakapacker/issues/80.
+const sassRule = webpackConfig.module.rules.find((rule) => ".sass".match(rule.test));
+const sassLoader = sassRule.use.find((use) => use.loader && use.loader.match(/sass-loader/));
+const sassOptions = {
+  // TODO: Add node_modules to load path?
+  quietDeps: true,
+  silenceDeprecations: ["global-builtin"]
+};
+
+sassLoader.options.sassOptions = merge(sassLoader.options.sassOptions, sassOptions);
+
+module.exports = webpackConfig;
